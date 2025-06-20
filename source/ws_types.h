d1add6cee467ab237725aa7e1a4cbd439d70299f
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-06-20 18:20:20 +0300

    feat: generate ws32 function signatures from a def file

--- source/ws_types.h
+++ source/ws_types.h
@@ -1,23 +1,14 @@
 #ifndef _WS_TYPES_H
 #define _WS_TYPES_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#include <sys/cdefs.h>
 
-/* Function pointer types for WinSock functions */
-  typedef unsigned long (*fn_htonl_t) (unsigned long);
-  typedef unsigned short (*fn_htons_t) (unsigned short);
-  typedef unsigned long (*fn_ntohl_t) (unsigned long);
-  typedef unsigned short (*fn_ntohs_t) (unsigned short);
-  typedef unsigned long (*fn_inet_addr_t) (const char *);
-  typedef char *(*fn_inet_ntoa_t) (struct in_addr);
-  typedef const char *(*fn_inet_ntop_t) (int, const void *, char *, size_t);
-  typedef int (*fn_inet_pton_t) (int, const char *, void *);
+__BEGIN_DECLS
 
-#ifdef __cplusplus
-}
-#endif
+#define WS_SYM(name, type, ret, ...) \
+    typedef ret (*type)(__VA_ARGS__);
+#include "ws_symbols.def"
+#undef WS_SYM
 
-#endif				/* _WS_TYPES_H */
+__END_DECLS
+#endif /* _WS_TYPES_H */
